9225c97d9cad6522e43128b11c2451dc91edfb22
David van Zyl <davidvanzyl@gmail.com>
2020-05-23 19:23:49 +0200

    3.6-38 Improved controls with lenient timers + ceiling check
     Improved controls with lenient timers + ceiling check

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,20 +5,48 @@ onready var debug_reset_position = get_node("../DebugResetPoint")
 
 var motion = Vector2(0,0)
 var lastPressed
+
 var is_jumping
+var buffer_jump = false
+var buffer_platform = false
+var pressed_jump_timer = null
+var on_platform_timer = null
+
+
 
 # Godot doesn't know what 'up' is. This determines if something is a floor etc..
 const UP = Vector2(0,-1)
 
-const SPEED = 1300
+const SPEED = 1000
 const GRAVITY = 100
-const JUMP_SPEED = 1700
+const JUMP_SPEED = 1900
 
 signal animate
+
+func _ready():
+	# Jump pressed timer setup
+	pressed_jump_timer = Timer.new()
+	pressed_jump_timer.set_timer_process_mode(1)
+	pressed_jump_timer.set_one_shot(true)
+	pressed_jump_timer.set_wait_time(0.15)
+	pressed_jump_timer.connect("timeout", self, "on_pressed_jump_timeout")
+	# Adds timer to process
+	add_child(pressed_jump_timer)
+	
+	# Platform left timer setup
+	on_platform_timer = Timer.new()
+	on_platform_timer.set_timer_process_mode(1)
+	on_platform_timer.set_one_shot(true)
+	on_platform_timer.set_wait_time(0.12)
+	on_platform_timer.connect("timeout", self, "on_platform_timeout")
+	# Adds timer to process
+	add_child(on_platform_timer)
 
 
 
 func _physics_process(delta):
+	print(on_platform_timer.get_time_left())
+	
 	# world effects and interactions
 	apply_gravity()
 	animate()
@@ -35,11 +63,13 @@ func _physics_process(delta):
 	move_and_slide(motion, UP)
 	
 func apply_gravity():
-	if not is_on_floor():
+	if is_on_floor():
 		# Have gravity affect player
-		motion.y += GRAVITY
-	else:
 		motion.y = 0
+	elif is_on_ceiling():
+		motion.y = GRAVITY
+	else:
+		motion.y += GRAVITY
 		
 
 func reset_player():
@@ -48,11 +78,34 @@ func reset_player():
 		set_position(debug_reset_position.get_position())
 	
 func jump():
+	# Timer check for remembering jump press just before hitting the ground
+	if Input.is_action_just_pressed("jump"):
+		pressed_jump_timer.start()
+		buffer_jump = true
+	
+	
 	if is_on_floor():
 		is_jumping = false
-	if Input.is_action_pressed("jump") and is_on_floor():
+		buffer_platform = true
+		on_platform_timer.start()
+		
+#		
+	if (Input.is_action_just_pressed("jump") and (is_on_floor() or buffer_platform==true)) or (is_on_floor() and buffer_jump == true) :
 		is_jumping=true
-		motion.y -= JUMP_SPEED	
+		motion.y = -JUMP_SPEED
+		buffer_platform = false
+		buffer_jump = false
+	
+
+# when timeout finishes, forget jump button was pressed recently
+func on_pressed_jump_timeout():
+	buffer_jump = false
+	
+# when timeout finishes, forget player was on platform recently
+func on_platform_timeout():
+	buffer_platform = false
+	print('completed jump buffer')
+	
 
 func move():
 	# if player presses the opposite direction while still holding previous direction:
