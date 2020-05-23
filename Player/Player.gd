d99c8e4b9df319ab0a480e334b363966e8e9fcfa
David van Zyl <davidvanzyl@gmail.com>
2020-05-23 14:11:18 +0200

    3.1-36: Player movement
    Adds initial player movement
    Adds temporary reset function
    Handles gravity and scene orientation (what is up)

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,11 +1,53 @@
 extends KinematicBody2D
 
+# temp reset point level 1 only
+onready var debug_reset_position = get_node("../DebugResetPoint")
+
 var motion = Vector2(0,0)
 var lastPressed
 
-const SPEED = 500
+# Godot doesn't know what 'up' is. This determines if something is a floor etc..
+const UP = Vector2(0,-1)
+
+const SPEED = 1000
+const GRAVITY = 50
+const JUMP_SPEED = 1000
+
+
+
 
 func _physics_process(delta):
+	# world effects and interactions
+	apply_gravity()
+	
+	# player actions
+	jump()
+	move()
+
+	#debug actions
+	reset_player()
+	
+	# move_and_slide needs to know what UP is to have floor logic work
+	move_and_slide(motion, UP)
+	
+func apply_gravity():
+	if not is_on_floor():
+		# Have gravity affect player
+		motion.y += GRAVITY
+	else:
+		motion.y = 0
+		
+
+func reset_player():
+	if Input.is_action_just_released("debug_reset"):
+		motion.y = 0
+		set_position(debug_reset_position.get_position())
+	
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED	
+
+func move():
 	# if player presses the opposite direction while still holding previous direction:
 	# make bunny go in opposite direction
 	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
@@ -21,4 +63,3 @@ func _physics_process(delta):
 		motion.x=SPEED
 	else:
 		motion.x=0
-	move_and_slide(motion)
